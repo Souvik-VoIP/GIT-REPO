c7235ea143239b0809b2f71bd7c43b54a0143b20
souvik_dutta <souvikvoip@gmail.com>
2024-05-27 15:15:36 +0530

    First commit

---
+++ Election.py
@@ -0,0 +1,19 @@
+#age = int(input(f'Enter your Age :'))
+#voter_id = True
+#if age >= 18:
+#    if voter_id == False:
+#        print (f'You are Eligible for Vote !')
+#    elif voter_id :
+#        print(f'First apply for Voter ID')
+#else:
+#    print(f'you are not Eligible')
+
+
+
+
+age = int(input(f'Enter your Age :'))
+voter_id = True
+if age >= 18 and voter_id:
+        print(f'You are Eligible for Vote !')
+else:
+    print(f'you are not Eligible')
